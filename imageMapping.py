569bea56112949aa020f32c74864a19c5b41a619
Niam Vaishnav <33373416+NiamV@users.noreply.github.com>
2019-11-16 16:20:00 +0000

    Added ability to generate 3 images

---
+++ imageMapping.py
@@ -0,0 +1,14 @@
+import math
+
+def images(n, imageCount):
+  numbers = [i for i in range(1,imageCount+1)]
+  img1 = math.ceil(n / ((imageCount-1)*(imageCount-2)))
+  numbers.remove(img1)
+
+  img2 = numbers[math.ceil((n % ((imageCount-1)*(imageCount-2))) / (imageCount-2))-1]
+  numbers.remove(img2)
+
+  img3 = numbers[math.ceil( (n % (imageCount-2)))-1]
+
+  return img1, img2, img3
+
--- niamPygame.py
+++ niamPygame.py
@@ -1,9 +1,12 @@
 import pygame
 import sys
 
+import imageMapping
+import random
+
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,900))
 
 clock = pygame.time.Clock()
 
@@ -11,18 +14,26 @@ done = False
 
 imageCount = 8
 
-my_image = pygame.image.load("static/Eq1.png")
-my_image_x = 0
-my_image_y = 0
-screen.blit(my_image, (my_image_x, my_image_y))
-
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        pass
+                        n = random.randint(1,imageCount*(imageCount-1)*(imageCount-2))
+                        threeImages =  imageMapping.images(n, imageCount)
+                        eq1 = pygame.image.load("static/Eq" + str(threeImages[0]) + ".png")
+                        eq1_x = 0
+                        eq1_y = 0
+                        screen.blit(eq1, (eq1_x, eq1_y))
+                        eq2 = pygame.image.load("static/Eq" + str(threeImages[1]) + ".png")
+                        eq2_x = 0
+                        eq2_y = 300
+                        screen.blit(eq2, (eq2_x, eq2_y))
+                        eq3 = pygame.image.load("static/Eq" + str(threeImages[2]) + ".png")
+                        eq3_x = 0
+                        eq3_y = 600
+                        screen.blit(eq3, (eq3_x, eq3_y))
 
         pygame.display.flip()
         
