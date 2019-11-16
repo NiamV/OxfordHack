9d69f67e1993d80291b6451e6539bca8f71c47b9
Niam Vaishnav <33373416+NiamV@users.noreply.github.com>
2019-11-16 16:54:59 +0000

    Updated so only shows one image at a time

--- niamPygame.py
+++ niamPygame.py
@@ -14,26 +14,27 @@ done = False
 
 imageCount = 8
 
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        n = random.randint(1,imageCount*(imageCount-1)*(imageCount-2))
-                        threeImages =  imageMapping.images(n, imageCount)
-                        eq1 = pygame.image.load("static/Eq" + str(threeImages[0]) + ".png")
-                        eq1_x = 0
-                        eq1_y = 0
-                        screen.blit(eq1, (eq1_x, eq1_y))
-                        eq2 = pygame.image.load("static/Eq" + str(threeImages[1]) + ".png")
-                        eq2_x = 0
-                        eq2_y = 300
-                        screen.blit(eq2, (eq2_x, eq2_y))
-                        eq3 = pygame.image.load("static/Eq" + str(threeImages[2]) + ".png")
-                        eq3_x = 0
-                        eq3_y = 600
-                        screen.blit(eq3, (eq3_x, eq3_y))
+equationsFile = open("static\Equations.txt", "r").read().splitlines()
 
-        pygame.display.flip()
+count = 0
+n = random.randint(1,imageCount*(imageCount-1)*(imageCount-2))
+
+while not done:
+    threeImages =  imageMapping.images(n, imageCount)
+    eqImg = [pygame.image.load("static/Eq" + str(threeImages[i]) + ".png") for i in range(0,3) ]
+    eqTxt = [equationsFile[threeImages[i]-1] for i in range(0,3) ]
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print(count)
+                    count += 1
+                    if count > 3:
+                        done = True
+                        break
+                    screen.fill((0,0,0)) 
+                    screen.blit(eqImg[count-1], (0, 0))
+
+    pygame.display.flip()
         
